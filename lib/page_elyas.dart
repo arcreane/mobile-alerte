8359d472193261cd0db6abb662c78d17bd059ade
AmelMahenni <93381563+AmelMahenni@users.noreply.github.com>
2021-11-11 16:31:44 +0100

    Update page_elyas.dart

--- lib/page_elyas.dart
+++ lib/page_elyas.dart
@@ -1,13 +1,72 @@
+import 'form.dart';
 import 'package:flutter/material.dart';
-class page_elyas  extends StatelessWidget {
+import 'ma_page.dart';
 
+class page_elyas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,// Cela permet d'enlever le debug de l'application par defaut
-      title: 'Liste des taches',
 
+      title: 'Liste des tâches',
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/fondd-2g.png"), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            title: Text('Liste des tâches',style: TextStyle(color: Colors.black, fontSize: 22.0, fontWeight: FontWeight.w600),),
+            centerTitle: true,
+            leading: IconButton(
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: (Colors.black),
 
+                ),
+                onPressed: () {
+                  Navigator.push( context,
+                    MaterialPageRoute(builder:(context)=>pageacceuil()),
+                  );
+
+                }),
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 70.0),
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                 children: <Widget>[
+                  SizedBox(height: 10.0),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 40),
+                      width: double.infinity,
+                      child: FlatButton(
+                        child: Text('afficher les listes', style: TextStyle(fontSize: 22),textAlign: TextAlign.center ),
+                        onPressed: () => {
+                          Navigator.push( context,
+                            MaterialPageRoute(builder:(context)=>formulaire()),
+                          ),
+                        },
+                        shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                        color: Color(0xFFcd9602),
+                        textColor: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
+
