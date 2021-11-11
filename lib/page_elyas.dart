10c26b5cabc16125fc4fb6acdb90b4491e476cc0
AmelMahenni <93381563+AmelMahenni@users.noreply.github.com>
2021-11-11 13:43:56 +0100

    Add files via upload

---
+++ lib/page_elyas.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class page_elyas  extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,// Cela permet d'enlever le debug de l'application par defaut
+      title: 'Liste des taches',
+
+
+    );
+  }
+}
