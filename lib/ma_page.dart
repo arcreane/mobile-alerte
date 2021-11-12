3b9634885cab03a0f73e17faa42c980a7b1b0b99
Celiachelli <93190978+Celiachelli@users.noreply.github.com>
2021-11-12 11:00:52 +0100

    Update ma_page.dart

--- lib/ma_page.dart
+++ lib/ma_page.dart
@@ -13,33 +13,34 @@ class pageacceuil extends StatelessWidget {
               decoration: new BoxDecoration(
                 image: new DecorationImage(
                   image: new AssetImage("images/Reminder.png"), fit: BoxFit.cover,),
-                  ),
-                ),
-            
-            //creer le bonton commencer qui à pour role de nous orienter vers les autres pages afin d'executer les taches de l'application
-            Align(
+              ),
+//creer le bonton commencer qui à pour role de nous orienter vers les autres pages afin d'executer les taches de l'application
+            ),Align(
               alignment: Alignment.bottomCenter,//centrer les boutons
-                child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 50, vertical: 40),//mettre les marges
-                width: double.infinity,
-                child: FlatButton(
-                child: Text('Commencer', style: TextStyle(fontSize: 22),textAlign: TextAlign.center ),//nommer le bouton
-                //clicker sur le bouton commencer
+              child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 50, vertical: 40),//mettre les marges
+              width: double.infinity,
+              child: FlatButton(
+              child: Text('Commencer', style: TextStyle(fontSize: 22),textAlign: TextAlign.center ),//nommer le bouton
+              //clicker sur le bouton commencer
                 onPressed: () => {
                 //faire appel à la page suivante
                     Navigator.push( context,
                     MaterialPageRoute(builder:(context)=>page_elyas()),
                     ),
-                  },
+              },
                 //mettre les formes arrondies au bouton et son style
-                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                color: Color(0xFFcd9602),
-                textColor: Colors.black,
-                  ),
-                )
+              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+              color: Color(0xFFcd9602),
+              textColor: Colors.black,
+              ),
               )
-            ],
-          )
-       );
-     }
+            )
+          ],
+        )
+
+    );
+
   }
+
+}
