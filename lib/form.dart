2e0276b5b6945238c292c5c75616791139446659
Celiachelli <93190978+Celiachelli@users.noreply.github.com>
2021-11-16 09:55:44 +0100

    Update form.dart
    code correction

--- lib/form.dart
+++ lib/form.dart
@@ -1,5 +1,4 @@
 
-//import 'ma_page.dart';
 import 'package:flutter/material.dart';
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/cupertino.dart';
@@ -140,7 +139,7 @@ class formulaire extends StatelessWidget {
                         border: OutlineInputBorder()
                         ),
 
-                        obscureText: true,
+                        
                         ),
                         SizedBox(height: 20.0),
 
